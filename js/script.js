f84a51f8ded17d463660489569e9d69cf36e0914
Yuki Honda <dahlia.15s2@gmail.com>
2023-06-18 02:05:05 +0800

    レスポンシブ化

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 $(document).ready(function(){
 	if (window.matchMedia('(min-width: 768px)').matches) {
     $(document).ready(function(){
-      
+
       $(".drawer").hide();
       $(function () {
           $(window).scroll(function () {
@@ -31,7 +31,23 @@ $(document).ready(function(){
 
 
 
+$(document).ready(function(){
+	if (window.matchMedia('(max-width: 767px)').matches) {
 
+var $slide = $(".slide")
+  .slick({
+    fade: true,    // fedeオン
+    speed: 1500,   // 画像切り替えにかかる時間（ミリ秒）
+    autoplaySpeed: 3000,   // 自動スライド切り替え速度
+    arrows: true,         // 矢印表示・非表示
+    autoplay: true,        // 自動再生
+    slidesToShow: 1,       // スライド表示数
+    slidesToScroll: 1,     // スライドする数
+    infinite: true         // 無限リピート オン・オフ
+  });
+
+}
+});
 
 
 
@@ -61,7 +77,8 @@ function omittedContent(string) {
 }
 
 
-
+$(document).ready(function(){
+	if (window.matchMedia('(min-width: 768px)').matches) {
 
 $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
@@ -74,4 +91,7 @@ $('.slider').slick({
     centerMode: true,//要素を中央ぞろえにする
     variableWidth: true,//幅の違う画像の高さを揃えて表示
     dots: false,//下部ドットナビゲーションの表示
+});
+
+}
 });
