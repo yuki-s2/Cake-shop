0ae149308044c779f25290c1eaa06b11f3c4b3b3
Yuki Honda <dahlia.15s2@gmail.com>
2023-06-17 08:47:15 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  $('.drawer').drawer();
+});
+
+
+
+$(document).ready(function(){
+	if (window.matchMedia('(min-width: 768px)').matches) {
+    $(document).ready(function(){
+      
+      $(".drawer").hide();
+      $(function () {
+          $(window).scroll(function () {
+              if ($(this).scrollTop() > 100) { //この数値で何pxスクロールしたらアイコンが現れるかを設定
+                  $('.drawer').fadeIn();
+              } else {
+                  $('.drawer').stop().fadeOut();
+              }
+          });
+         //$('#back-top a').click(function () {
+         //    $('body,html').animate({
+         //        scrollTop: 0  //戻る位置
+         //    }, 600); //トップへ戻る速さを設定。数値が小さいほどスピードアップ。
+         //    return false;
+         //});
+      });
+    });
+    
+	}
+});
+
+
+
+
+
+
+
+// document.querySelectorAllでli要素を全て取得する。
+let contents = document.querySelectorAll('.news__item-title');
+// liは2つあるのでcontentsという変数に格納しました。
+
+// contentsをforEachで一つずつ取得、引数が一つの場合は()を省略できる。(content) => {}の()を省略している。
+contents.forEach(content => {
+  content.textContent = omittedContent(content.textContent);
+  // content.textContentで文字列を取得できる（loop1回目の場合は"テストの文章1です"が入っている)
+});
+
+// 引数でcontent.textContentをstringという変数名で受け取る。
+function omittedContent(string) {
+  // 定数で宣言
+  const MAX_LENGTH = 53;
+
+  // もしstringの文字数がMAX_LENGTH（今回は10）より大きかったら末尾に...を付け足して返す。
+  if (string.length > MAX_LENGTH) {
+
+    // substr(何文字目からスタートするか, 最大値);
+    return string.substr(0, MAX_LENGTH) + '...';
+  }
+  //　文字数がオーバーしていなければそのまま返す
+  return string;
+}
+
+
+
+
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    speed: 500,//スライドのスピード。初期値は300。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    centerMode: true,//要素を中央ぞろえにする
+    variableWidth: true,//幅の違う画像の高さを揃えて表示
+    dots: false,//下部ドットナビゲーションの表示
+});
